c66b3eb0ebb2878ef9663b810bd58548f158cebc
Alex Martin <sharkfinpro@hotmail.com>
2023-06-01 16:13:46 -0700

    page additions

--- src/components/explore.js
+++ src/components/explore.js
@@ -8,12 +8,12 @@ export default function Explore() {
                 <div className={"card"}>
                     <h1>Activities</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto aut, delectus deserunt error ex exercitationem explicabo nesciunt reprehenderit suscipit. Corporis culpa doloremque dolorum ducimus eos esse optio perspiciatis ut.</p>
-                    <a className={"btn"}>Learn More</a>
+                    <a className={"btn"} href={"/activities"}>Learn More</a>
                 </div>
                 <div className={"card"}>
                     <h1>Logistics</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto aut, delectus deserunt error ex exercitationem explicabo nesciunt reprehenderit suscipit. Corporis culpa doloremque dolorum ducimus eos esse optio perspiciatis ut.</p>
-                    <a className={"btn"}>Learn More</a>
+                    <a className={"btn"} href={"/logistics"}>Learn More</a>
                 </div>
                 <div className={"card"}>
                     <h1>aaa</h1>
---
+++ src/pages/activities.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Activities() {
+    return (
+        <div>
+            <h1>Activities!</h1>
+        </div>
+    );
+}
