45f04f5820786dc0aeb49c8bb1930ac9d2181708
Alex Martin <sharkfinpro@hotmail.com>
2023-06-14 22:36:58 -0700

    Homepage container
     - Limit width of display to 2500px, add black bars on sides when too wide

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,41 +9,43 @@ import * as indexStyles from "../css/index.module.css";
 
 const Index = () => {
   return (
-    <div className={indexStyles.contentWrapper}>
-      <header>
-        <div className={indexStyles.headerContent}>
-          <div>
-            <h1>Expedition Fairbanks</h1>
-            <p>North to the Future</p>
+    <div className={indexStyles.contentContainer}>
+      <div className={indexStyles.contentWrapper}>
+        <header>
+          <div className={indexStyles.headerContent}>
+            <div>
+              <h1>Expedition Fairbanks</h1>
+              <p>North to the Future</p>
+            </div>
           </div>
-        </div>
-        <StaticImage className={indexStyles.headerBackground} src={"../../static/images/lights_1080.webp"} alt={"Northern Lights"} placeholder={"blurred"} />
-      </header>
-      <section className={indexStyles.intro}>
-        <div className={indexStyles.introContentWrapper}>
-          <div className={indexStyles.introContent}>
-            <h2>Section G15's Conclave</h2>
-            <p>August 11<sup>th</sup> - 13<sup>th</sup>, 2023</p>
-          </div>
-        </div>
-        <div className={indexStyles.introLogoWrapper}>
-          <StaticImage className={indexStyles.introLogo} src={"../../static/images/wask_512.png"} alt={"Section G15 Logo"} placeholder={"blurred"} />
-        </div>
-      </section>
-      <section className={indexStyles.intro}>
-        <div className={indexStyles.introLogoWrapper}>
-          <div className={indexStyles.introContent}>
-            <h2>Hosted at Lost Lake Scout Camp</h2>
-            <p>Fairbanks, Alaska</p>
-          </div>
+          <StaticImage className={indexStyles.headerBackground} src={"../../static/images/lights_1080.webp"} alt={"Northern Lights"} placeholder={"blurred"} />
+        </header>
+        <section className={indexStyles.intro}>
+          <div className={indexStyles.introContentWrapper}>
+            <div className={indexStyles.introContent}>
+              <h2>Section G15's Conclave</h2>
+              <p>August 11<sup>th</sup> - 13<sup>th</sup>, 2023</p>
+            </div>
           </div>
           <div className={indexStyles.introLogoWrapper}>
-          <StaticImage className={indexStyles.introLogo} src={"../../static/images/logoYellow_512.png"} alt={"Expedition Fairbanks Logo"} placeholder={"blurred"} />
-        </div>
-      </section>
-      <DaysUntil />
-      <Explore />
-      <Footer />
+            <StaticImage className={indexStyles.introLogo} src={"../../static/images/wask_512.png"} alt={"Section G15 Logo"} placeholder={"blurred"} />
+          </div>
+        </section>
+        <section className={indexStyles.intro}>
+          <div className={indexStyles.introLogoWrapper}>
+            <div className={indexStyles.introContent}>
+              <h2>Hosted at Lost Lake Scout Camp</h2>
+              <p>Fairbanks, Alaska</p>
+            </div>
+          </div>
+          <div className={indexStyles.introLogoWrapper}>
+            <StaticImage className={indexStyles.introLogo} src={"../../static/images/logoYellow_512.png"} alt={"Expedition Fairbanks Logo"} placeholder={"blurred"} />
+          </div>
+        </section>
+        <DaysUntil />
+        <Explore />
+        <Footer />
+      </div>
     </div>
   );
 };
