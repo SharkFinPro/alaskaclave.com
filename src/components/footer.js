d0055112059f804e154556f8d863b7b315d8a50e
Alex Martin <sharkfinpro@hotmail.com>
2023-06-15 01:37:07 -0700

    update footer content
     - added section website
     - added social icons (Instagram + Facebook)

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import * as footerStyle from "../css/footer.module.css";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function Footer() {
   return (
@@ -8,6 +9,15 @@ export default function Footer() {
         <div className={footerStyle.content}>
           <h1>Expedition Fairbanks | alaskaclave.com</h1>
           <p><a href={"mailto:info@alaskaclave.com"}>info@alaskaclave.com</a></p>
+          <p><a href={"https://sectiong15.oa-bsa.org/"} target={"__blank"} rel={"external"}>sectionG15.oa-bsa.org</a></p>
+          <div className={footerStyle.icons}>
+            <a href={"https://www.facebook.com/sectiong15"} title={"Section G15 Facebook"} target={"__blank"} rel={"external"}>
+              <StaticImage src={"../../static/images/facebookIcon.svg"} alt={"Facebook Icon"} />
+            </a>
+            <a href={"https://www.instagram.com/sectiong15/"} title={"Section G15 Instagram"} target={"__blank"} rel={"external"}>
+              <StaticImage src={"../../static/images/instagramIcon.svg"} alt={"Instagram Icon"} />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
