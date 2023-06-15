c1871451fdac110cd628731d8b3d9dfaaaeded10
Alex Martin <sharkfinpro@hotmail.com>
2023-06-15 13:24:34 -0700

    Cleanup (#7)
    * don't preload italic fonts
    
    * Register button border radius change
    
    vw > em
    
    * Transition on button
    
    * Update explore.module.css
    
    * Update daysUntil.module.css

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -12,27 +12,11 @@ export const onRenderBody = ({ setHeadComponents }) => {
     />,
     <link
       rel="preload"
-      href="/fonts/Museo_Slab_500italic.otf"
-      as="font"
-      type="font/otf"
-      crossOrigin="anonymous"
-      key="museoSlab500i"
-    />,
-    <link
-      rel="preload"
       href="/fonts/MuseoSans_500.otf"
       as="font"
       type="font/otf"
       crossOrigin="anonymous"
       key="museoSans500"
-    />,
-    <link
-      rel="preload"
-      href="/fonts/MuseoSans_500_Italic.otf"
-      as="font"
-      type="font/otf"
-      crossOrigin="anonymous"
-      key="museoSans500i"
     />
   ]);
 }
