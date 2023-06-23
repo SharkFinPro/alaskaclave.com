052668a567c0753d7b02c24ece7ec63f1a7b69fc
Alex Martin <sharkfinpro@hotmail.com>
2023-06-23 13:51:32 -0700

    Add sizes

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -19,6 +19,7 @@ export default function Shop() {
             image {
               gatsbyImageData
             }
+            sizes
           }
         }
       }
@@ -36,9 +37,14 @@ export default function Shop() {
             <div key={node.name}>
               <div className={shopStyles.productCard}>
                 {/*<GatsbyImage className={shopStyles.productCardImage} image={node.image?.gatsbyImageData} alt={node.name} />*/}
-                <GatsbyImage className={shopStyles.productCardImage} image={products[0].node.image?.gatsbyImageData} alt={node.name} />
+                <GatsbyImage className={shopStyles.productCardImage} image={products[6].node.image?.gatsbyImageData} alt={node.name} />
                 <div className={shopStyles.productCardText}>
                   <h1 className={shopStyles.productCardName}>{node.name}<span className={shopStyles.productCardPrice}>${node.price}</span></h1>
+                  <p className={shopStyles.productCardSizes}>
+                    {node.sizes?.map((size) => (
+                      <span>{size}</span>
+                    ))}
+                  </p>
                   <p className={shopStyles.productCardDescription}>{node.description?.description}</p>
                 </div>
                 <div className={shopStyles.productCardCart}>
