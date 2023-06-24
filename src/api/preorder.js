015535a724bd020b0787f4f2b880da32ac59f6d2
Alex Martin <sharkfinpro@hotmail.com>
2023-06-24 11:44:17 -0700

    Remove 0s from logged order
     - Only allow name, email, phone, cart to be written to database

--- src/api/preorder.js
+++ src/api/preorder.js
@@ -30,12 +30,46 @@ export default async function handler(req, res) {
     return;
   }
 
+  let shookCart = {...body.cart};
+
+  try {
+    for (let item in shookCart) {
+      if (typeof shookCart[item] === "number") {
+        if (shookCart[item] === 0) {
+          delete shookCart[item];
+        }
+      } else {
+        for (let size in shookCart[item]) {
+          if (shookCart[item][size] === 0) {
+            delete shookCart[item][size];
+          }
+        }
+        if (Object.keys(shookCart[item]).length === 0) {
+          delete shookCart[item];
+        }
+      }
+    }
+  } catch (e) {
+    res.status(400).send("Cart is incorrectly formatted");
+    return;
+  }
+
+  if (Object.keys(shookCart).length === 0) {
+    res.status(400).send("There are no items in the cart");
+    return;
+  }
+
   try {
     const db = client.db("shop");
     const collection = db.collection("preorders");
 
     await client.connect();
-    await collection.insertOne(body);
+    await collection.insertOne({
+      name: body.name,
+      email: body.email,
+      phone: body.phone,
+      cart: shookCart
+    });
   } catch (error) {
     console.log(error);
     res.status(400).send("Error writing to database")
