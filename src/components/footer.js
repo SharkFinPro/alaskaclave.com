bffa4dbfd7562e6f677778bf027a869a5794b48f
Alex Martin <sharkfinpro@hotmail.com>
2023-06-25 17:22:20 -0700

    h1 > h5
     - margin updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
     <footer className={footerStyle.wrapper}>
       <div className={footerStyle.container}>
         <div className={footerStyle.content}>
-          <h1>Expedition Fairbanks | alaskaclave.com</h1>
+          <h5>Expedition Fairbanks | alaskaclave.com</h5>
           <p>
             <a href={"mailto:info@alaskaclave.com"}>info@alaskaclave.com</a>
           </p>
